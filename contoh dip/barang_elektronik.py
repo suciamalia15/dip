6effc7b031374eae3ed771badb207e338f701ce8
suciamalia15 <suciamalia394@gmail.com>
2022-05-31 00:34:30 +0700

    perubahan suci

---
+++ contoh dip/barang_elektronik.py
@@ -0,0 +1,11 @@
+from abc import ABC, abstractmethod
+
+class BarangElektronik(ABC):
+    
+    @abstractmethod
+    def beroperasi(self):
+        pass
+    
+    @abstractmethod
+    def berhenti(self):
+        pass
---
+++ contoh dip/kipas_angin.py
@@ -0,0 +1,9 @@
+from barang_elektronik import BarangElektronik
+
+class KipasAngin(BarangElektronik):
+    
+    def beroperasi(self):
+        print("Kipas angin berputar")
+        
+    def berhenti(self):
+        print("Kipas angin berhenti berputar")
